8b1f839137c0638713b90b89ad70b458b8663417
duncan.macleod <duncan.macleod@ligo.org>
2023-01-18 11:28:57 +0000

    update build args for cross compiled builds

--- recipe/build.sh
+++ recipe/build.sh
@@ -13,6 +13,7 @@ cmake \
 	${SRC_DIR} \
 	${CMAKE_ARGS} \
 	-DCMAKE_BUILD_TYPE:STRING=RelWithDebInfo \
+	-DCMAKE_CROSSCOMPILING_EMULATOR:STRING="${CMAKE_CROSSCOMPILING_EMULATOR}" \
 	-DCMAKE_DISABLE_FIND_PACKAGE_Doxygen:BOOL=true \
 	-DCMAKE_OSX_ARCHITECTURES:STRING="${OSX_ARCH}" \
 ;
@@ -21,7 +22,7 @@ cmake \
 cmake --build . --parallel ${CPU_COUNT} --verbose
 
 # test
-if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" ]]; then
+if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR}" != "" ]]; then
 	ctest --parallel ${CPU_COUNT} --verbose
 fi
 
